a93401cf313ac7d3885241b2e360603f9912a205
Nikolay Dimitriev <dimitrievk56@gmail.com>
2022-11-26 21:20:27 +0300

    create styles for main block and media query

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,18 @@
+import { format } from "date-fns";
 import { FooterProps } from "./FooterProps";
+import cn from "classnames";
 import styles from "./Footer.module.css";
 
-export const Footer = ({ ...props }: FooterProps) => {
-  return <footer {...props}>Footer</footer>;
+export const Footer = ({ className, ...props }: FooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div> NDTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
