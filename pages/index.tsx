367ae5ee6fcc88dd1312f46cabd2e09569413a46
Nikolay Dimitriev <dimitrievk56@gmail.com>
2022-12-06 23:44:41 +0300

    works with ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { GetStaticProps } from "next";
 import { useState } from "react";
+import axios from "axios";
 import { Htag, Button, Paragraph, Tag, Rating } from "../components";
 import { withLayout } from "../layout/Layout";
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home() {
+function Home({ menu }: HomeProps) {
   const [rating, setRating] = useState<number>(0);
+
   return (
     <>
       <Htag tag="h1">Test</Htag>
@@ -26,8 +30,32 @@ function Home() {
         Primary href
       </Tag>
       <Rating isEditable rating={rating} setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    { firstCategory }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
